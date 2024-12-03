92ad75809a46468ce28b7f6f6dea437807b3dd4d
markmcmahon <mark.mcmahon@sagesure.com>
2024-12-03 13:16:39 -0500

    day 2 part 1

---
+++ 02/02.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fi, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer fi.Close()
+
+	scanner := bufio.NewScanner(fi)
+
+	safe := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fields := strings.Fields(line)
+
+		var record []int
+		for _, field := range fields {
+			num, err := strconv.Atoi(field)
+			if err != nil {
+				continue
+			}
+			record = append(record, num)
+		}
+
+		if isMonotonic(record) && isSafeDiff(record) {
+			safe += 1
+		}
+
+	}
+
+	fmt.Println(safe)
+
+}
+
+func isMonotonic(record []int) bool {
+
+	isIncreasing := true
+	isDecreasing := true
+
+	for i := 1; i < len(record); i++ {
+		if record[i] < record[i-1] {
+			isIncreasing = false
+		}
+		if record[i] > record[i-1] {
+			isDecreasing = false
+		}
+	}
+
+	return isIncreasing || isDecreasing
+
+}
+
+func isSafeDiff(record []int) bool {
+
+	isSafeDiff := true
+
+	for i := 1; i < len(record); i++ {
+
+		diff := record[i] - record[i-1]
+		absdiff := int(math.Abs(float64(diff)))
+		if absdiff < 1 || absdiff > 3 {
+			isSafeDiff = false
+		}
+	}
+	return isSafeDiff
+}
